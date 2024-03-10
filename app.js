56c8c9bb7c97b0f82f235264103d15778a063ae7
Pujon Das <67597048+pujondas2@users.noreply.github.com>
2024-03-10 20:55:31 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const content = document.querySelector('.content');
+
+const title = document.querySelector('.title');
+
+const buttons = document.querySelector('.btn-group');
+
+const yesBtn = document.querySelector('#yes-btn');
+
+const noBtn = document.querySelector('#no-btn');
+
+yesBtn.addEventListener('click', () => {
+    title.innerHTML = 'Thanks For Accepting!'
+    buttons.style.display = 'none';
+})
+
+const contentRect = content.getBoundingClientRect();
+
+const noBtnRect = noBtn.getBoundingClientRect();
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (contentRect.width - noBtnRect.width));
+
+    const j = Math.floor(Math.random() * (contentRect.height - noBtnRect.height));
+
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+})
